4d9d6eefa77b8eb6d2bfd1302e58d6e7825b836b
fakaradut <furkanahmet.karadut@gmail.com>
2022-11-26 19:11:54 +0300

    url is valid

--- app/utilities/functions.js
+++ app/utilities/functions.js
@@ -1,11 +1,22 @@
 const url = require('url')
 
 function getUrlParams(fullUrl) {
-    if(!fullUrl) {
+    if(!fullUrl || !isValidHttpUrl(fullUrl)) {
         return false;
     }
     return url.parse(fullUrl, true);
 }
+
+function isValidHttpUrl(string) {
+    let url;
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;
+    }
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
+
 module.exports = {
     getUrlParams,
 }
