23d09b2f6e714ea7453455a5e7e20cd7b50ea24d
gulalicelik <gulalicelikmi@gmail.com>
2022-11-26 17:38:11 +0300

    get comments beta

--- app/app.js
+++ app/app.js
@@ -5,9 +5,12 @@ const app = express()
 const routeManager = require('./route/route.manager.js')
 const bodyParser = require('body-parser')
 const db = require("./models/index")
+const cors = require('cors')
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(cors())
 
 
 db.sequelize.sync()
--- app/route/comment.route.js
+++ app/route/comment.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const commentController = require('../controller/comment.controller.js');
 
-router.get('/', commentController.printCommentHello)
+router.get('/', commentController.getComments)
 router.post('/', commentController.createNewComment)
 
 
