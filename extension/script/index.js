91ea4edca22525df35dc5467d54318ec57a554df
Mehmetcngvrcn <97710290+Mehmetcngvrcn@users.noreply.github.com>
2022-11-28 00:13:26 +0300

    selector

--- extension/script/index.js
+++ extension/script/index.js
@@ -3,6 +3,8 @@ console.log("::: index.js loaded");
 var modalOpen = false;
 var modalId = "RModal";
 var modalClass = "RModal";
+var selector = null;
+var submit = false;
 // const modalId = new Date().getTime() + "-RModal";
 document.body.innerHTML += `
     <div id="${modalId}" class="${modalClass}">
@@ -15,17 +17,50 @@ document.getElementById(`${modalId}-submit`).addEventListener("click", () => {
     "::: Comment",
     document.getElementById(`${modalClass}-comment-text`).value
   );
+  fetch("https://clfow.herokuapp.com/comment", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      address: window.location.href,
+      comment: document.getElementById(`${modalClass}-comment-text`).value,
+      selector: selector,
+    }),
+  })
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (res) {
+      console.log(res);
+    });
   console.log("Submit");
+  document.getElementById(modalId).style.display = "none";
+  submit = true;
 });
 
 window.addEventListener("scroll", (e) => {
-  document.getElementById(modalId).style.display = "none";
-  modalOpen = false;
+  if (!submit) {
+    document.getElementById(modalId).style.display = "none";
+    modalOpen = false;
+  }
 });
 
 document.querySelectorAll("*").forEach((el, i) => {
   el.addEventListener("mouseover", (event) => {
     if (!modalOpen) {
+      if (
+        event.target.classList.value
+          .replace("hoverElRM", "")
+          .trim()
+          .replace(" ", ".")
+      ) {
+        selector = event.target.classList.value
+          .replace("hoverElRM", "")
+          .trim()
+          .replace(" ", ".");
+      }
       event.target.classList.add("hoverElRM");
       document.querySelector(".hoverElRM").addEventListener(
         "contextmenu",
