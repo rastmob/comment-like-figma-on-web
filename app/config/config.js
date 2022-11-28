75f049b2465df3fde4c28fa4fd8bf226f2c4fbd2
gulalicelik <gulalicelikmi@gmail.com>
2022-11-28 15:41:25 +0300

    edit env variable name

--- app/config/config.js
+++ app/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
         "dialect" : "mysql"
     },
     "test"       : {
-        "username": process.env.DB_USERNAME,
+        "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_NAME,
         "host"    : process.env.DB_HOST,
