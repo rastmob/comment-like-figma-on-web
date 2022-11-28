60969a7f62b23f1c763b904eb7771bc469c33e7e
gulalicelik <gulalicelikmi@gmail.com>
2022-11-28 22:11:50 +0300

    change endpoint routes for post reqs

--- app/route/comment.route.js
+++ app/route/comment.route.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const commentController = require('../controller/comment.controller.js');
 
-router.get('/', commentController.getComments)
-router.post('/', commentController.createNewComment)
+router.post('/get', commentController.getComments)
+router.post('/add', commentController.createNewComment)
 
 
 module.exports = router;
