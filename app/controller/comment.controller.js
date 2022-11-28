212dba64fb419786093433df6fe7940692f56f9d
gulalicelik <gulalicelikmi@gmail.com>
2022-11-28 23:13:07 +0300

    edit getComment api

--- app/controller/comment.controller.js
+++ app/controller/comment.controller.js
@@ -1,54 +1,88 @@
 const db = require("../models/index")
 const helper = require("../utilities/functions")
 let url;
+let site_id, page_id;
 const getComments = async (req, res) => {
+    try {
 
-    url = helper.getUrlParams(req.body.address)
+        url = helper.getUrlParams(req.body.address)
 
-   if(!url) {
-        res.send({
-            "status" : 500,
-            "message": "address parameters is required.",
-        })
-        return;
-   }
-    var site_id, page_id;
-    db.sites.findOne({
-        where: {domain: url.host},
-    }).then(site => {
-            site_id = site.id
-            db.pages.findOne({
-                where: {url: url.path, site_id: site_id},
-            }).then(page => {
-                page_id = page.id
-                db.comments.findAll({
-                    where: {page_id: page_id},
-                }).then(comments => {
-                    res.send({
-                        "status"  : 200,
-                        "message" : "Comments fetched successfully",
-                        "comments": comments,
-                    });
-                })
+        if (!url) {
+            res.send({
+                "status" : 500,
+                "message": "address parameters is required. Please check your address and enter  a valid address",
             })
+            return;
         }
-    ).catch(e => {
-        res.send({
-            "status" : 500,
-            "message": e.message,
-        })
-    })
 
+        await db.sites.findOne({
+            where: {domain: url.host, active: 1, deleted: 0},
+        }).then(site => {
+            if (!site) {
+                res.send({
+                    "status" : 500,
+                    "message": "Site not found.",
+                })
+                return;
+            }
+            site_id = site.id;
+        }).catch(err => {
+            res.send({
+                "status" : 500,
+                "message": err.message,
+            })
+        });
 
+        await db.pages.findOne({
+            where: {site_id: site_id, url: url.path, active: 1, deleted: 0},
+        }).then(page => {
+            if (!page) {
+                res.status(200).send({
+                    "status" : 500,
+                    "message": "No comments found for this page.",
+                });
+                return;
+            }
+            page_id = page.id;
+
+        }).catch(err => {
+            res.send({
+                "status" : 500,
+                "message": err.message,
+            })
+        });
+
+        await db.comments.findAll({
+            where: {page_id: page_id},
+        }).then(comments => {
+            res.status(200).send({
+                "status"  : 200,
+                "message" : "Comments fetched successfully",
+                "comments": comments,
+            });
+        }).catch(err => {
+            res.send({
+                "status" : 500,
+                "message": err.message,
+            })
+        });
+    } catch (e) {
+        res.send(
+            {
+                "status" : 500,
+                "message": e.message,
+            }
+        )
+    }
 }
 
 
 const createNewComment = async (req, res) => {
     url = helper.getUrlParams(req.body.address);
-    if(!url) {
+    if (!url) {
         res.send({
             "status" : 500,
-            "message": "address parameters is required.",
+            "message": "address parameters is required. Please check your address and enter  a valid address",
         })
         return;
     }
