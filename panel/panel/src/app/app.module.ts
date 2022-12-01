e1fbea62c483dde1b2b513d72ab17f0c94b4a93c
burcineren <burcin.ern969@gmail.com>
2022-12-01 18:09:17 +0300

    devextreme module included

--- panel/panel/src/app/app.module.ts
+++ panel/panel/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DevExtremeModule,DxButtonModule } from 'devextreme-angular';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DevExtremeModule,
+    DxButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
