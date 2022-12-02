0c8fabcedc7c7e1bf01ad2c8bc81a87a1701deb7
burcineren <burcin.ern969@gmail.com>
2022-12-02 09:26:15 +0300

    devextreme datatable

--- panel/panel/src/app/app.component.ts
+++ panel/panel/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Service } from './app.service';
+import DataSource from 'devextreme/data/data_source';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Service]
 })
 export class AppComponent {
-  helloWorld() {
-    alert('Hello world!');
-}
+  dataSource: DataSource;
+  
+
+  constructor(service: Service) {
+    this.dataSource = service.getDataSource();
+  }
 }
---
+++ panel/panel/src/app/app.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import 'devextreme/data/odata/store';
+import DataSource from 'devextreme/data/data_source';
+
+@Injectable()
+export class Service {
+  getDataSource() {
+    return new DataSource({
+      store: {
+        type: 'odata',
+        url: 'https://js.devexpress.com/Demos/SalesViewer/odata/DaySaleDtoes',
+        key: 'Id',
+        beforeSend(request) {
+          request.params.startDate = '2020-05-10';
+          request.params.endDate = '2020-05-15';
+        },
+      },
+    });
+  }
+}
