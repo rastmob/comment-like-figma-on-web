aa632563fdd95f227baa12167906ab8938717fb3
gulalicelik <gulalicelikmi@gmail.com>
2022-12-24 14:06:26 +0300

    edit sequelize config

--- app/config/config.js
+++ app/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     "development": {
-        "username": "root",
-        "password": "",
-        "database": "clfow_dev",
-        "host"    : "127.0.0.1",
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host"    : process.env.DB_HOST,
         "dialect" : "mysql"
     },
     "test"       : {
@@ -14,10 +14,10 @@ module.exports = {
         "dialect" : "mysql"
     },
     "production" : {
-        "username": "root",
-        "password": "",
-        "database": "clfow_prod",
-        "host"    : "127.0.0.1",
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host"    : process.env.DB_HOST,
         "dialect" : "mysql"
     }
 }
