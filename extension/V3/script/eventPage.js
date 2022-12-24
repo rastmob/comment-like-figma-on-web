e68f7259405e5ec0bdb71e0445d60d17af7dfbb6
gulalicelik <gulalicelikmi@gmail.com>
2022-12-24 15:38:55 +0300

    manifest version upgrade

---
+++ extension/V3/script/eventPage.js
@@ -0,0 +1,65 @@
+const menuItem = {
+  id: "addComment-CLFW",
+  title: "Add Comment",
+  contexts: ["all"],
+};
+chrome.contextMenus.create(menuItem, contextMenuCallback);
+
+chrome.contextMenus.onClicked.addListener((clickData) => {
+  if (clickData.menuItemId == "addComment-CLFW") {
+    var menuItemOptions = {
+      type: "basic",
+      iconUrl: "images/favicon-32x32.png",
+      title: "",
+      message: "",
+    };
+    chrome.notifications.create("createComment-CLFW", menuItemOptions);
+    chrome.tabs.executeScript({
+      file: "script/index.js",
+    });
+  }
+});
+
+function contextMenuCallback() {
+  console.log("contextMenuCallback");
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete") {
+    console.log(":::Page Complete");
+    chrome.storage.local.get("rmInject", function (result) {
+      console.log("rmInject", result);
+      if (typeof result.rmInject == "boolean" && result.rmInject) {
+        console.log(":::Page result.rmInject");
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+          fetch("https://clfowapi.rastmobile.com/comment/get", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ address: tabs[0].url }),
+          })
+            .then((response) => {
+              console.log("loggg::::", response);
+              return response.status == 200 ? response.text() : null;
+            })
+            .then((data) => {
+              console.log(data);
+              if (data && JSON.parse(data).status == 200) {
+                chrome.tabs.executeScript(
+                  {
+                    code: "var RMComment = " + data + ";",
+                  },
+                  function () {
+                    chrome.tabs.executeScript({ file: "script/page-load.js" });
+                  }
+                );
+              } else {
+                console.log(`ERR : ${JSON.parse(data).status}`);
+              }
+            });
+        });
+      }
+    });
+  }
+});
---
+++ extension/V3/script/index.js
@@ -0,0 +1,96 @@
+console.log("::: index.js loaded");
+
+var modalOpen = false;
+var modalId = "RModal";
+var modalClass = "RModal";
+var selector = null;
+var submit = false;
+// const modalId = new Date().getTime() + "-RModal";
+document.body.innerHTML += `
+    <div id="${modalId}" class="${modalClass}">
+      <textarea id="${modalClass}-comment-text" name="text" class="comment-input" placeholder="Comment"></textarea>
+      <button id="${modalId}-submit" type="button">Submit</button>
+    </div>`;
+document.getElementById(`${modalId}-submit`).addEventListener("click", () => {
+  console.log(
+    "::: Comment",
+    document.getElementById(`${modalClass}-comment-text`).value
+  );
+  fetch("https://clfowapi.rastmobile.com/comment/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      address: window.location.href,
+      comment: document.getElementById(`${modalClass}-comment-text`).value,
+      selector: selector,
+    }),
+  })
+    .then(function (res) {
+      console.log(res);
+      window.location.reload();
+    })
+    .catch(function (res) {
+      console.log(res);
+      window.location.reload();
+    });
+  console.log("Submit");
+  document.getElementById(modalId).style.display = "none";
+  submit = true;
+});
+
+window.addEventListener("scroll", (e) => {
+  if (!submit) {
+    document.getElementById(modalId).style.display = "none";
+    modalOpen = false;
+  }
+});
+
+document.querySelectorAll("*").forEach((el, i) => {
+  el.addEventListener("mouseover", (event) => {
+    if (!modalOpen) {
+      selector = generateQuerySelector(event.target);
+      console.log(selector);
+      event.target.classList.add("hoverElRM");
+      document.querySelector(".hoverElRM").addEventListener(
+        "contextmenu",
+        function (e) {
+          if (!modalOpen && document.getElementById(modalId)) {
+            document.getElementById(modalId).style.display = "block";
+            modalOpen = true;
+          }
+          e.preventDefault();
+        },
+        false
+      );
+    } else {
+      event.target.classList.remove("hoverElRM");
+    }
+  });
+
+  el.addEventListener("mouseout", (event) => {
+    event.target.classList.remove("hoverElRM");
+  });
+});
+
+var generateQuerySelector = function (el) {
+  if (el.tagName.toLowerCase() == "html") return "HTML";
+  var str = el.tagName;
+  str += el.id != "" ? "#" + el.id : "";
+  if (el.className && typeof el.className === "string") {
+    var classes = el.className
+      .split(/\s/)
+      .filter((data) => data != "hoverElRM");
+    for (var i = 0; i < classes.length; i++) {
+      str += "." + classes[i];
+    }
+  }
+  if (str.endsWith(".")) {
+    str = str.slice(0, -1);
+  }
+  return (generateQuerySelector(el.parentNode) + " > " + str)
+    .replaceAll(". ", " ")
+    .replaceAll("..", ".")
+    .replaceAll("  ", " ");
+};
