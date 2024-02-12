58a9052ab7942a5f500f231f07ae1f5f6e4c217a
malp <mehmet.alp@rastmobile.com>
2024-02-12 13:40:06 +0300

    fix: remove outside URL for comments image

--- extension/script/page-load.js
+++ extension/script/page-load.js
@@ -2,7 +2,7 @@ if (RMComment?.comments) {
   RMComment.comments.forEach((data) => {
     if (document.querySelector(data.selector)) {
       var img = document.createElement("img");
-      img.src = "https://img.icons8.com/color/512/comments--v1.png";
+      img.src = "../images/comments--v1.png";
       img.className = "RMCOMMENTIMG";
       img.setAttribute("title", data.comment);
       document.querySelector(data.selector).before(img);
