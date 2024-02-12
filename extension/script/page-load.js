08753b8f0ab96ae6171cbf4f9ed9e46bdd7a9ce5
malp <mehmet.alp@rastmobile.com>
2024-02-12 14:06:49 +0300

    fix: improve design of popup page

--- extension/script/page-load.js
+++ extension/script/page-load.js
@@ -2,7 +2,7 @@ if (RMComment?.comments) {
   RMComment.comments.forEach((data) => {
     if (document.querySelector(data.selector)) {
       var img = document.createElement("img");
-      img.src = "../images/comments--v1.png";
+      img.src = "https://www.rastmobile.com/assets/images/comments--v1.png";
       img.className = "RMCOMMENTIMG";
       img.setAttribute("title", data.comment);
       document.querySelector(data.selector).before(img);
